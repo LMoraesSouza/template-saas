55813621d107b4b2adefef316cf276853ee18965
Nithron <lucas.moraes.souza@hotmail.com>
2025-04-17 09:34:13 -0300

    fix type error payments/page - testeId

--- app/(project)/payments/page.tsx
+++ app/(project)/payments/page.tsx
@@ -9,10 +9,10 @@ export default function Payments() {
     return (
         <div className="flex flex-col gap-10 items-center justify-center h-screen">
             <h1 className="text-4xl font-bold">Pagamentos</h1>
-            <button className="border rounded-md px-1" onClick={() => createPaymentStripeCheckout({testeId: 123})}>
+            <button className="border rounded-md px-1" onClick={() => createPaymentStripeCheckout({testeId: '123'})}>
                 Criar Pagamento Stripe      
             </button>
-            <button className="border rounded-md px-1" onClick={() => createSubscriptionStripeCheckout({testeId: 123})}>
+            <button className="border rounded-md px-1" onClick={() => createSubscriptionStripeCheckout({testeId: '123'})}>
                 Criar Assinatura Stripe      
             </button>
             <button className="border rounded-md px-1" onClick={handleCreateStripePortal}>
