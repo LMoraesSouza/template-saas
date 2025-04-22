68aabd6f2ff10e616675585dadc9277850987ce4
Nithron <lucas.moraes.souza@hotmail.com>
2025-04-22 12:32:58 -0300

    added resend

--- app/server/mercado-pago/handle-payments.ts
+++ app/server/mercado-pago/handle-payments.ts
@@ -1,3 +1,4 @@
+import resend from "@/app/lib/resend";
 import { PaymentResponse } from "mercadopago/dist/clients/payment/commonTypes";
 
 export async function handleMercadoPagoPayment(paymentData: PaymentResponse) {
@@ -5,5 +6,18 @@ export async function handleMercadoPagoPayment(paymentData: PaymentResponse) {
     const userEmail = metadata.user_email;
     const testeId = metadata.teste_id;
     
-    console.log("PAGAMENTO COM SUCESSO", {userEmail, testeId, paymentData})
+    console.log("PAGAMENTO COM SUCESSO", {userEmail, testeId, paymentData});
+
+    const { data, error } = await resend.emails.send({
+        from: 'Acme <onboarding@resend.dev>',
+        to: [userEmail],
+        subject: 'Hello world',
+        text: "Pagamento Realizado com sucesso!"
+      });
+    
+    if (error) {
+        console.error(error);
+    }
+
+    console.log(data)
 }
